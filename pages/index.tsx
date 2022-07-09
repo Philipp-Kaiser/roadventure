ed2310487b3b2a4ba3a0e32bc9db7b76b2949bc4
Philipp Kaiser <philipp.kaiser@frachtwerk.de>
2022-07-09 22:05:17 +0200

    create hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,28 @@ import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <main>
+    <div>
       <Head>
         <title>Roadventure</title>
       </Head>
 
-      <h2>Hit the road!</h2>
-    </main>
+      <section>
+        <h2>Plan your roadtrips</h2>
+        <p>
+          Of course roadtrips should be spontaneous. But its way better if you have a lot of options to discover right
+          from the start, right?
+        </p>
+
+        <h2>Discover hidden gems</h2>
+        <p>
+          Fly over germany and get some inspirations for new places besides the road. Find amazing places that are just
+          waiting to impress you.
+        </p>
+
+        <h2>Be prepared</h2>
+        <p>Make sure you dont forget any equipment anymore. Use the travellist to keep everything you need on board.</p>
+      </section>
+    </div>
   )
 }
 
